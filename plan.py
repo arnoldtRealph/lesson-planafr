83fcde3eb169ee4abf93b22781aa5b771f1aac95
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-04-13 22:31:29 +0200

    Initial commit

--- plan.py
+++ plan.py
@@ -1,94 +1,64 @@
 import streamlit as st
-import docx
 import os
-from docx.enum.text import WD_ALIGN_PARAGRAPH
-from docx.shared import Pt
-from docx.oxml.ns import qn
-import getpass
+import io
+import docx
 
-# Set up the document style
-document = docx.Document()
-document.styles['Normal'].font.name = 'Arial'
-document.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), 'Arial')
-document.styles['Normal'].font.size = Pt(12)
-document.styles['Heading 1'].font.bold = True
-document.styles['Heading 1'].font.size = Pt(16)
-document.styles['Heading 2'].font.bold = True
-document.styles['Heading 2'].font.size = Pt(14)
-document.styles['Heading 3'].font.bold = True
-document.styles['Heading 3'].font.size = Pt(12)
+# Set page title and icon
+st.set_page_config(page_title="Lesson Plan Creator", page_icon=":books:")
 
-st.title("HOЁRSKOOL SAUL DAMON")
-st.header("LESBEPLANNER")
-# Define the grade levels
-grade_levels = ['GRAAD 9', 'GRAAD 10', 'GRAAD 11', 'GRAAD 12']
+# Create input fields
+st.write("HOЁRSKOOL SAUL DAMON")
+st.write("LESBEPLANNER")
+st.write("Please fill out the following fields to create a new lesson plan:")
+st.write("")
+subject = st.text_input("Subject")
+lesson_title = st.text_input("Lesson Title")
+grade = st.selectbox("Grade", ["Grade 9", "Grade 10", "Grade 11", "Grade 12"])
+lesson_date = st.date_input("Lesson Date")
+lesson_objective = st.text_area("Lesson Objective")
+lesson_activities = st.text_area("Lesson Activities")
+materials_needed = st.text_area("Materials Needed")
+homework = st.text_area("Homework")
+notes = st.text_area("Notes")
+teacher_name = st.text_input("Teacher Name")
+teacher_surname = st.text_input("Teacher Surname")
+st.write("")
 
-# Set up the sidebar
-st.sidebar.title("DAAGLIKE BEPLANNER")
-selected_grade = st.sidebar.selectbox("KIES N GRAAD", grade_levels)
-
-# Define the lesson plan inputs
-subject_name = st.text_input("VAK")
-lesson_title = st.text_input("LES TITEL")
-lesson_date = st.date_input("LES DATUM")
-lesson_objective = st.text_input("LES DOELWIT")
-lesson_activities = st.text_area("LEERDER AKTIWITEITE")
-lesson_materials = st.text_area("MATERIAAL BENODIG")
-lesson_homework = st.text_area("HUISWERK")
-lesson_notes = st.text_area("NOTAS")
-
-st.write("Designed by Mr. A.R Visagie @ Saul Damon High School")
-# Set up the save button
-if st.button("Save", key='save_button'):
-    # Check if the file already exists
-    username = getpass.getuser()
-    file_path = os.path.expanduser(f"C:/Users/{username}/Desktop/{lesson_title}.docx")
-    while os.path.exists(file_path):
-        st.warning(f"A file with the name '{lesson_title}.docx' already exists on your desktop. Please choose a different name.")
-        lesson_title = st.text_input("Lesson Title")
-        file_path = os.path.expanduser(f"C:/Users/{username}/Desktop/{lesson_title}.docx")
-        
-    # Add the lesson plan to the document
-    p = document.add_paragraph(selected_grade, style='Heading 1')
-    p = document.add_paragraph()
-    p.add_run(f"Subject: ").bold = True
-    p.add_run(f"{subject_name}").bold = True
-    p = document.add_paragraph(lesson_title, style='Heading 1')
-    
-    # Add the lesson date
-    date_str = lesson_date.strftime('%A, %B %d, %Y')
-    document.add_paragraph(date_str, style='Heading 2')
-
-# Add the lesson objective, activities, materials, and homework
-    document.add_heading("LES DOELWIT", level=2)
+# Create save button
+if st.button("Create Lesson Plan"):
+    # Create a new Word document
+    document = docx.Document()
+    # Add input values to the document
+    document.add_heading(subject.upper(), level=0)
+    document.add_paragraph("")
+    document.add_paragraph("Lesson Title: " + lesson_title)
+    document.add_paragraph("Grade: " + grade)
+    document.add_paragraph("Lesson Date: " + str(lesson_date))
+    document.add_paragraph("")
+    document.add_heading("Lesson Objective", level=1)
     document.add_paragraph(lesson_objective)
-    
-    document.add_heading("LEERDER AKTIWITEITE", level=2)
+    document.add_heading("Lesson Activities", level=1)
     document.add_paragraph(lesson_activities)
-    
-    document.add_heading("MATERIAAL BENODIG", level=2)
-    document.add_paragraph(lesson_materials)
-    
-    document.add_heading("HUISWERK", level=2)
-    document.add_paragraph(lesson_homework)
-    
-    # Add the notes
-    document.add_heading("NOTAS", level=2)
-    document.add_paragraph(lesson_notes)
-
-    # Add the teacher's signature line
+    document.add_heading("Materials Needed", level=1)
+    document.add_paragraph(materials_needed)
+    document.add_heading("Homework", level=1)
+    document.add_paragraph(homework)
+    document.add_heading("Notes", level=1)
+    document.add_paragraph(notes)
     document.add_paragraph("")
-    document.add_paragraph("")
-    document.add_paragraph("")
-    p = document.add_paragraph()
-    p.alignment = WD_ALIGN_PARAGRAPH.RIGHT
-    p.add_run("___________________________").bold = True
-    p.add_run("\nTeacher's Signature").bold = True
+    document.add_paragraph("Teacher Name: " + teacher_name)
+    document.add_paragraph("Teacher Surname: " + teacher_surname)
+    document.add_paragraph("Teacher Signature: ")
 
-    # Save the document
-    file_path = os.path.join(os.path.expanduser('~'), 'Desktop', f"{lesson_title}.docx")
-    document.save(file_path)
-    st.success("File saved successfully!")
-
-    
-   
+    # Save document to BytesIO object
+    with io.BytesIO() as output:
+        document.save(output)
+        output.seek(0)
+        # Create download button
+        st.download_button(
+            label="Download Lesson Plan",
+            data=output,
+            file_name="lesson_plan.docx",
+            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        )
+        st.success("Your lesson plan has been created. Click the download button to save the file.")
