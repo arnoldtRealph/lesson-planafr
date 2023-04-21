b40da5c9035f544d8abfa539b81db85bf018a1b0
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-04-21 16:28:17 +0200

    header

--- pages/SGA EN KURRIKULUM AFHANDELINGS INSTRUMENTE.py
+++ pages/SGA EN KURRIKULUM AFHANDELINGS INSTRUMENTE.py
@@ -1,10 +1,14 @@
-import streamlit as st 
+import streamlit as st
 import webbrowser
 
 # Here is the title of the page
 st.title("Klik hier onder vir SGA en KURR. AFHANDELING")
 
-st.write("[KLIK HIER](https://drive.google.com/drive/folders/1pi7vv0BvG5-s9o-j27jFWoiXI82Agarg?usp=share_link)")
+if st.button("KLIK HIER"):
+    # Open the link in a new tab
+    webbrowser.open_new_tab("https://drive.google.com/drive/folders/1pi7vv0BvG5-s9o-j27jFWoiXI82Agarg?usp=share_link")
+    st.success("Jou versoek was suksesvol!")
+
 
 
 
