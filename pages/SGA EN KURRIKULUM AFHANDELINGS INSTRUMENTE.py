58df7eae7390a5c8c05716bf4f7d3e6bb152bb66
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-04-21 16:43:30 +0200

    Button

--- pages/SGA EN KURRIKULUM AFHANDELINGS INSTRUMENTE.py
+++ pages/SGA EN KURRIKULUM AFHANDELINGS INSTRUMENTE.py
@@ -4,10 +4,9 @@ import webbrowser
 # Here is the title of the page
 st.title("Klik hier onder vir SGA en KURR. AFHANDELING")
 
-if st.button("KLIK HIER"):
-    # Open the link in a new tab
-    webbrowser.open_new_tab("https://drive.google.com/drive/folders/1pi7vv0BvG5-s9o-j27jFWoiXI82Agarg?usp=share_link")
-    st.success("Jou versoek was suksesvol!")
+
+st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1pi7vv0BvG5-s9o-j27jFWoiXI82Agarg?usp=share_link)")
+
 
 
 
