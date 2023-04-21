4c028c199537b17fa97a709c7fa3826119f43447
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-04-21 08:08:47 +0200

    SGA en Kur

---
+++ pages/SGA EN KURRIKULUM AFHANDELINGS INSTRUMENTE.py
@@ -0,0 +1,4 @@
+import streamlit as st 
+
+st.title("Klik hier onder vir SGA en KURR. AFHANDELING")
+st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1pi7vv0BvG5-s9o-j27jFWoiXI82Agarg?usp=share_link)")
