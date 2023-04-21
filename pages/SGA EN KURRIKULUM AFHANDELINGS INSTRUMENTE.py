e7cef42c2164a6cfe06b56c38d62d40f7add7ac6
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-04-21 10:34:31 +0200

    Button

--- pages/SGA EN KURRIKULUM AFHANDELINGS INSTRUMENTE.py
+++ pages/SGA EN KURRIKULUM AFHANDELINGS INSTRUMENTE.py
@@ -7,7 +7,6 @@ st.title("Klik hier onder vir SGA en KURR. AFHANDELING")
 # Define the URL that the button should redirect to
 url = "https://drive.google.com/drive/folders/1pi7vv0BvG5-s9o-j27jFWoiXI82Agarg?usp=share_link"
 
-
 # Create a button with a label "Go to Example.com"
 if st.button("KLIK HIER"):
     webbrowser.open_new_tab(url)
