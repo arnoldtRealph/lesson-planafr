11d7d70ef3e8cd15793c67598e36a30c71efb847
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-04-21 10:43:09 +0200

    Button removed

--- pages/SGA EN KURRIKULUM AFHANDELINGS INSTRUMENTE.py
+++ pages/SGA EN KURRIKULUM AFHANDELINGS INSTRUMENTE.py
@@ -4,12 +4,8 @@ import webbrowser
 # Here is the title of the page
 st.title("Klik hier onder vir SGA en KURR. AFHANDELING")
 
-# Define the URL that the button should redirect to
-url = "https://drive.google.com/drive/folders/1pi7vv0BvG5-s9o-j27jFWoiXI82Agarg?usp=share_link"
+st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1pi7vv0BvG5-s9o-j27jFWoiXI82Agarg?usp=share_link)")
 
-# Create a button with a label "Go to Example.com"
-if st.button("KLIK HIER"):
-    webbrowser.open_new_tab(url)
 
 
 
