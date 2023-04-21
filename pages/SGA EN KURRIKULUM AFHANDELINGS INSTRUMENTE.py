d1b2b15a3fafbfbcc3b3d0ddf011b69f4cb39004
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-04-21 11:39:27 +0200

    Klik hier

--- pages/SGA EN KURRIKULUM AFHANDELINGS INSTRUMENTE.py
+++ pages/SGA EN KURRIKULUM AFHANDELINGS INSTRUMENTE.py
@@ -4,7 +4,7 @@ import webbrowser
 # Here is the title of the page
 st.title("Klik hier onder vir SGA en KURR. AFHANDELING")
 
-st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1pi7vv0BvG5-s9o-j27jFWoiXI82Agarg?usp=share_link)")
+st.write("[KLIK HIER](https://drive.google.com/drive/folders/1pi7vv0BvG5-s9o-j27jFWoiXI82Agarg?usp=share_link)")
 
 
 
