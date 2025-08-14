6d39c79ea65cc34c471d4f16dcb632137f6468af
Arnoldt <127623684+arnoldtRealph@users.noreply.github.com>
2025-08-14 11:22:16 +0200

    Update 4_🗃️PRE_AND POST MOD._TOOL.py

--- pages/4_🗃️PRE_AND POST MOD._TOOL.py
+++ pages/4_🗃️PRE_AND POST MOD._TOOL.py
@@ -6,7 +6,7 @@ import requests
 from streamlit_extras.app_logo import add_logo
 import streamlit_analytics
 
-streamlit_analytics.start_tracking()
+
 # add app logo
 add_logo("IMAGES/wapen.png", height=150)
 
@@ -34,4 +34,3 @@ st.write("[KLIK HIER](https://drive.google.com/drive/folders/1HGDkPeJgMlTLYuYpj2
 st_lottie(lottie_coding, height = 200 , key="coding")
 
 
-streamlit_analytics.stop_tracking()
